bafe946ecf7efbaa4f4e57cd43d85d32ab32379b
denis daros <denisv.daros@gmail.com>
2023-10-12 17:45:59 -0300

    adicionando renderização condicional

--- src/components/LuckyNumbers.tsx
+++ src/components/LuckyNumbers.tsx
@@ -1,17 +1,22 @@
 import luckyNumber from './utils/luckyNumber';
 
 function LuckyNumber() {
+  const show = false;
   return (
     <div>
       <h2 className="subtitle">Seus números da sorte da Mega-Sena são:</h2>
-      <ul>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-        <li>{luckyNumber()}</li>
-      </ul>
+      {show ? (
+        <ul>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+          <li>{luckyNumber()}</li>
+        </ul>
+      ) : (
+        <h3 className="subtitle">Não há números para serem exibidos 😢</h3>
+      )}
     </div>
   );
 }
